956a465b60820f12057a214d15da8219a75dbdba
André Formento <andreformento.sc@gmail.com>
2016-02-16 21:43:27 -0200

    controller

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-boot-heroku2
-spring-boot-heroku2
+
+Push no Heroku: git push heroku master
--- src/main/java/com/formento/spring/boot/config/SpringBootHeroku2Application.java
+++ src/main/java/com/formento/spring/boot/config/SpringBootHeroku2Application.java
@@ -1,9 +1,11 @@
 package com.formento.spring.boot.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class SpringBootHeroku2Application {
 
     public static void main(String[] args) {
