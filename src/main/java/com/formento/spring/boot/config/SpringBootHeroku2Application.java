e7bb6a0c268f63cd69f281d9d7428bef72e64c30
André Formento <andreformento.sc@gmail.com>
2016-02-16 21:46:14 -0200

    controller 2

--- src/main/java/com/formento/spring/boot/config/SpringBootHeroku2Application.java
+++ src/main/java/com/formento/spring/boot/config/SpringBootHeroku2Application.java
@@ -1,11 +1,15 @@
 package com.formento.spring.boot.config;
 
+import com.formento.spring.boot.controller.HelloController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@ComponentScan(basePackageClasses = {
+        HelloController.class})
 public class SpringBootHeroku2Application {
 
     public static void main(String[] args) {
